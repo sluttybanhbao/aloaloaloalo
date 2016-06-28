a6a010fea0278254f142165ef748ca99709dbd1a
sluttybanhbao <banhbaoglory@gmail.com>
2016-06-28 21:11:25 +1100

    Create données.py

---
+++ données.py
@@ -0,0 +1,29 @@
+# Nombre de coups par partie
+nb_coups = 8
+
+# Nom du fichier stockant les scores
+nom_fichier_scores = "scores"
+
+# Liste des mots du pendu
+liste_mots = [
+    "armoire",
+    "boucle",
+    "buisson",
+    "dérivee",
+    "chaise",
+    "primitive",
+    "vaisala",
+    "fichier",
+    "garage",
+    "glace",
+    "journal",
+    "claquette",
+    "lampe",
+    "liste",
+    "montagne",
+    "remise",
+    "asymptote",
+    "taxi",
+    "vampire",
+    "chien",
+
